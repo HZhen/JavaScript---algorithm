9b8cefdaa4c64cc76c997fa182f127f566782301
huangzhen@17m.club <1656471829@qq.com>
2020-02-04 17:29:06 +0800

    重复的子字符串和正则表示式匹配的实现

---
+++ test/regexp/lesson2.test.js
@@ -0,0 +1,13 @@
+import regModeStr from '../../code/regexp/lesson2'
+
+test('regModeStr', () => {
+  expect(regModeStr('aaa', 'caa*')).toBe(false)
+})
+
+test('regModeStr:2', () => {
+  expect(regModeStr('aaabc', 'a*bc')).toBe(true)
+})
+
+test('regModeStr:3', () => {
+  expect(regModeStr('aaabc', 'a*.*')).toBe(true)
+})
