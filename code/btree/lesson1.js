b7d211a9689bd93272e45fdeada4e295462998fd
huangzhen@17m.club <1656471829@qq.com>
2020-02-07 21:25:26 +0800

    对称二叉树和搜索二叉树的实现

---
+++ code/btree/lesson1.js
@@ -0,0 +1,61 @@
+class Node {
+  constructor (val) {
+    this.val = val
+    this.left = this.right = undefined
+  }
+}
+
+// 构建二叉树
+class Tree {
+  constructor (data) {
+    // 临时存储所有节点，方便寻找父子节点
+    let nodeList = []
+    // 顶节点
+    let root
+    for (let i = 0, len = data.length; i < len; i++) {
+      let node = new Node(data[i])
+      nodeList.push(node)
+      if (i > 0) {
+        // 计算当前节点属于那一层,例如：节点5，返回2
+        let n = Math.floor(Math.sqrt(i + 1))
+        // 记录当前层的起始点，3
+        let q = Math.pow(2, n) - 1
+        // 记录上一层的起始点，1
+        let p = Math.pow(2, n - 1) - 1
+        // 找到当前节点的父节点，2
+        let parent = nodeList[p + Math.floor((i - q) / 2)]
+        // 将当前节点和上一层的父节点做关联
+        if (parent.left) {
+          parent.right = node
+        } else {
+          parent.left = node
+        }
+      }
+    }
+    root = nodeList.shift()
+    nodeList.length = 0
+    return root
+  }
+  // 判断当前二叉树是否为对称的
+  static isSymmetry (root) {
+    if (!root) {
+      return true
+    }
+    let walk = (left, right) => {
+      if (!left && !right) {
+        return true
+      }
+      if ((left && !right) || (!left && right) || (left.val !== right.val)) {
+        return false
+      }
+      return walk(left.left, right.right) && walk(left.right, right.left)
+    }
+    return walk(root.left, root.right)
+  }
+}
+
+export default Tree
+
+export {
+  Node
+}
